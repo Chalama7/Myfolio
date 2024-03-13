8788b6ae7ec9f7102c82b5249e36d6c19bd4d57a
CHALAMAREDDY <chalamav262000@gmail.com>
2024-03-13 19:02:56 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Responsive navbar toggle
+const navbarToggle = document.getElementById('navbar-toggle');
+const navbarMenu = document.getElementById('navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('hidden');
+});
